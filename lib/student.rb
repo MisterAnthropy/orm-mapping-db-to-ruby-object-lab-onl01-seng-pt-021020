3dff27883e1dddc96d90fd56e1b52c59db8747ae
Tyler Croley <tjcroley@outlook.com>
2020-04-21 20:34:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE students.grade < 12 
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
   
   def save
     sql = <<-SQL
