f97ed7c152acfb869bd49c055de61eb23aee5296
Tyler Croley <tjcroley@outlook.com>
2020-04-21 20:41:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,11 +56,11 @@ class Student
     SELECT * 
     FROM students 
     WHERE grade = 10 
-    LIMIT ?
+    LIMIT 1
     SQL
     DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
-    end.first 
+    end
   end
   
   def save
